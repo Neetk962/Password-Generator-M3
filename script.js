79872d13f744dcff9cba1b6c86190716e6af2b49
Ravneet Kaur <neet.k962@gmail.com>
2023-08-31 13:05:23 -0700

    //fixed js

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
-// Assignment code here
 
 
-// Get references to the #generate element
+// adding arrays 
+var passwordsource = ""
+var finalpassword = ""
+var lowercasealphabet ="abcdefghijklmnopqrstuvwxyz"
+var specialcharacters="!@#$%^&*"
+var uppercasealphabet ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+//used if statements, used for loops to loop password source
+
+function generatePassword() {
+  var userChoice = parseInt(window.prompt("Enter a number between 8-128 characters"));
+  if (userChoice>8 && userChoice <128) {
+    var userChoice2 = window.confirm("include lowercase, uppercase, and special characters")
+    passwordsource+=lowercasealphabet
+
+    if (userChoice2) {
+    passwordsource+=specialcharacters
+    passwordsource+=uppercasealphabet
+      
+    // Used math.random to numbers for final password 
+    }
+    for (let index = 0; index < userChoice; index++) {
+      finalpassword+= passwordsource.charAt(Math.floor(Math.random()*passwordsource.length))
+    }
+// used an alert to notify user that the number had to be in between 8-128
+return finalpassword
+  } else {
+    alert("Please enter a number between 8-128!")
+    return
+  }
+  
+}
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
